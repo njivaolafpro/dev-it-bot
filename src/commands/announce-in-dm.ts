ab785b30287046d9aae375ca8cf23cc2e02be5e4
njivaolafpro <njivaolaf.pro@gmail.com>
2022-12-20 17:38:48 +0100

    fix: rolename

--- src/commands/announce-in-dm.ts
+++ src/commands/announce-in-dm.ts
@@ -15,7 +15,7 @@ export default {
                 .setRequired(true)
         )
         .addRoleOption(option =>
-            option.setName("roleName")
+            option.setName("rolename")
                 .setDescription("Describe your announcement.")
         )
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
@@ -24,17 +24,17 @@ export default {
         const { options, guild } = interaction;
         const title = options.get("title")?.value as string | undefined;
         const message = options.get("message")?.value as string | undefined;
-        const roleName = options.get("roleName")?.value as string | undefined;
+        const rolename = options.get("rolename")?.value as string | undefined;
 
-        if (!title || !message || !roleName) {
+        if (!title || !message || !rolename) {
             await interaction.reply({ content: 'Please make sure that all parameters are sent!', ephemeral: true });
             console.warn('Missing data');
             return;
         }
 
-        console.log('we are checking for role ->', { roleName });
+        console.log('we are checking for role ->', { rolename });
         const allMembers = await guild?.members.fetch();
-        const membersFound = allMembers?.filter(m => m.roles.cache.find(r => r.name === roleName));
+        const membersFound = allMembers?.filter(m => m.roles.cache.find(r => r.name === rolename));
         console.log('found members ->', membersFound?.map(m => m).length);
         const embed = new EmbedBuilder()
             .setColor("Green")
